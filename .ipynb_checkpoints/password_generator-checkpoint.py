34016edf0b2ea85f54093cb3c61721b069682511
hdkatta1 <harshithadevik@gmail.com>
2023-03-12 22:55:28 -0300

    a successful generator, still has to work on the front-end

---
+++ .ipynb_checkpoints/password_generator-checkpoint.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+length = int(input("Enter the length of your password: "))
+chars = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation.replace("'", "")
+
+password = "".join(random.choices(chars + digits + symbols, k=length)) 
+#random.choices function to randomly select characters from these three sets of characters
+#k=length argument specifies how many characters to choose
+
+print("Your generated password is: ", password)
+
+from flask import Flask, render_template
+def index():
+    return render_template('fpg.html')
